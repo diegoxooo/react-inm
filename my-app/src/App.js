ffb050b1333f36f3acacca21f9639f867a72eaef
Diego Rodríguez <58006463+diegoxooo@users.noreply.github.com>
2024-03-14 15:09:30 +0000

    complementos implementados

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Array from './array/Array';
+import Calendar from './calendar/Calendar'
+import User from './user/User'
+
 
 function App() {
+  const name = "Diego";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <User />
+      <Calendar />
+      <Array />
+      <div>Hello {name}</div>
+    </>
   );
 }
 
