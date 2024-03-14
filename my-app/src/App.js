3aa9987771d265dd02a5382702a9fd59a070ff8c
Diego Rodríguez <58006463+diegoxooo@users.noreply.github.com>
2024-03-14 19:22:29 +0000

    principal

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,21 @@
 import Array from './array/Array';
 import Calendar from './calendar/Calendar'
 import User from './user/User'
-
+import Counter from './counter/Counter'
+import ArrayState from './arraystate/ArrayState';
+import Timer from './timer/Timer'
 
 function App() {
   const name = "Diego";
   return (
     <>
+      <Timer />
+      <ArrayState />
+      <Counter />
       <User />
       <Calendar />
       <Array />
+      
       <div>Hello {name}</div>
     </>
   );
