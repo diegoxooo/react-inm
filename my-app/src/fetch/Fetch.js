cb38f98b647c3e16bcc7a53d6944739dce41e667
Diego Rodríguez <58006463+diegoxooo@users.noreply.github.com>
2024-03-18 18:36:37 +0000

    creacion de evento con "CustomEvent"

--- my-app/src/fetch/Fetch.js
+++ my-app/src/fetch/Fetch.js
@@ -8,15 +8,38 @@ export default function Fetch(){
     if(error){
         return <div>{error}</div>
     }
-    return <>
+
+    function eventCarrito(pizza){
+        return new CustomEvent("carrito", {
+            detail:pizza,
+            composed:true,
+            bubbles:true
+        })
+    }
+
+    function handlerClick(ev){
+        ev.stopPropagation();
+
+        const node = ev.nativeEvent.composedPath().find(n=>n.dataset && 'id' in n.dataset)
+
+        if(node){
+            const {id} = node.dataset
+            const pizza = data.find(p=>p.id===id)
+
+            pizza && node.dispatchEvent(eventCarrito(pizza))
+        }
+    }
+
+    return <div onClick={handlerClick}>
         {data.map(({ id, name, price })=>{
             return (
                 <div key={id}>
                     <div>{id}</div>
                     <div>{name}</div>
                     <div>{price}</div>
+                    <button data-id={id}>Comprar</button>
                 </div>
             )
         })}
-    </>
+    </div>
 }
