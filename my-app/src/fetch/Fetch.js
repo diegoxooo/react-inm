6b517453e46b84c8ae25fe6a5d3d00388cd4d778
Diego Rodríguez <58006463+diegoxooo@users.noreply.github.com>
2024-03-18 16:32:50 +0000

    api rest

---
+++ my-app/src/fetch/Fetch.js
@@ -0,0 +1,22 @@
+import { useFetch } from "./useFetch";
+
+export default function Fetch(){
+    const {loading, error, data}=useFetch("http://127.0.0.1:3000/pizzas",[])
+    if(loading){
+        return <div>Loading...</div>
+    }
+    if(error){
+        return <div>{error}</div>
+    }
+    return <>
+        {data.map(({ id, name, price })=>{
+            return (
+                <div key={id}>
+                    <div>{id}</div>
+                    <div>{name}</div>
+                    <div>{price}</div>
+                </div>
+            )
+        })}
+    </>
+}
---
+++ my-app/src/fetch/useFetch.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(url, initialValue){
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [data, setData] = useState(initialValue);
+
+    useEffect(()=>{
+        fetch(url)
+            .then(response=>response.json()) // Primero debemos transformar la respuesta "Response" de fetch a JSON
+            .then(data=>setData(data)) // En caso de OK
+            .catch(error=>setError(error)) // Cuando hay error
+            .finally(()=>setLoading(false)) // Cuando se ha cargado
+    },[url]) // Asi hacemos que "useEffect" se ejecute solo cuando la url cambie
+
+    return {
+        loading,
+        error,
+        data
+    }
+} 
