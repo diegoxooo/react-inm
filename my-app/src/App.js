0d777518df3dc16ec5c20a1a898248e0190cae69
Diego Rodríguez <58006463+diegoxooo@users.noreply.github.com>
2024-03-18 16:33:12 +0000

    app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,21 @@
-import Array from './array/Array';
-import Calendar from './calendar/Calendar'
-import User from './user/User'
-import Counter from './counter/Counter'
-import ArrayState from './arraystate/ArrayState';
-import Timer from './timer/Timer'
+// import Array from './array/Array';
+// import Calendar from './calendar/Calendar'
+// import User from './user/User'
+// import Counter from './counter/Counter'
+// import ArrayState from './arraystate/ArrayState';
+// import Timer from './timer/Timer'
+// import Parent from './children/Parent';
+
+import Fetch from "./fetch/Fetch";
 
 function App() {
-  const name = "Diego";
+  return (
+    <Fetch />
+  )
+  /*const name = "Diego";
   return (
     <>
+      <Parent />
       <Timer />
       <ArrayState />
       <Counter />
@@ -18,7 +25,7 @@ function App() {
       
       <div>Hello {name}</div>
     </>
-  );
+  );*/
 }
 
 export default App;
